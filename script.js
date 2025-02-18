2111a001352677f17fc843860071a7a7f5e9a28f
Brian Masaki <masakibrian@gmail.com>
2025-02-19 01:50:09 +0300

    updated structure, styling and functionality

--- script.js
+++ script.js
@@ -6,38 +6,17 @@ const showMenuContent = () => {
   menuContent.classList.toggle('show');
 }
 
-
 // Close the menu if the user clicks outside of it
-
-
 window.onclick = (e) => {
   if (!e.target.matches('#menu-icon')){
 menuContent.classList.remove('show');
   }
 }
 
-
-// Below is another way to implement the above code (to close the menu if the user clicks outside of it)
-
-// window.onclick = (e) => {
-//   if (!e.target.matches('.menu-btn')) {
-//     let dropdowns = document.getElementsByClassName("menu-content");
-//     let i;
-//     for (i = 0; i < dropdowns.length; i++) {
-//       let openDropdown = dropdowns[i];
-//       if (openDropdown.classList.contains('show')) {
-//         openDropdown.classList.remove('show');
-//       }
-//     }
-//   }
-// } 
-
-
-// Making the current year span element dynamic with javascript getFullYear method
+// Making the current year span element dynamic with the JavaScript getFullYear method
 const currentYear = document.getElementById('current-year');
 
 const showCurrentYear = () => {
-  currentYear.innerHTML = new Date().getFullYear();
-  // console.log(new Date().getFullYear())
+  currentYear.innerText = new Date().getFullYear();
 }
 showCurrentYear();
